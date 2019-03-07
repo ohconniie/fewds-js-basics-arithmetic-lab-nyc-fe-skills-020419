2ee629bc7c895adc728119564115cc2c15898672
Connie Chan <conniechan.10@gmail.com>
2019-03-07 01:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random() * (20 - 0) + 0;
 
-const flooredRandomInteger = Math.floor(randomNumber);
+const randomInteger = Math.floor(randomNumber);
 
 // Write your code below this line!
